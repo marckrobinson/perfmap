09c96d690baa51c42181b484a6096f51d4e10a0e
Marck Robinson <marckr@microsoft.com>
2015-09-07 09:59:18 -0700

    Test using npm packages and express

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Node World!\n'+JSON.stringify(process));
-}).listen(port);
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 50000;
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+
+// var http = require('http')
+// var port = process.env.PORT || 50000;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello Node World!\n');
+// }).listen(port);
